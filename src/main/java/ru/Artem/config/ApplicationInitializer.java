89b38c584591c5af405459b87a3944b5304b4b72
Artem Pupyshev <hdboomone.art@gmail.com>
2025-05-05 18:18:35 +0300

    Starting Config

---
+++ src/main/java/ru/Artem/config/ApplicationInitializer.java
@@ -0,0 +1,17 @@
+package ru.Artem.config;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+
+    @Override
+    public void onStartup(ServletContext servletContext){
+
+
+    }
+}
---
+++ src/main/java/ru/Artem/config/WebConfig.java
@@ -0,0 +1,14 @@
+package ru.Artem.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.Artem")
+public class WebConfig  implements WebMvcConfigurer {
+
+
+}
