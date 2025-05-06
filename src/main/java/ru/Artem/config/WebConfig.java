bcdcfd919e2f49cdca47f63c8d9dcd11de815332
Artem Pupyshev <hdboomone.art@gmail.com>
2025-05-06 12:07:25 +0300

    First Web page

--- src/main/java/ru/Artem/config/WebConfig.java
+++ src/main/java/ru/Artem/config/WebConfig.java
@@ -1,14 +1,25 @@
 package ru.Artem.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("ru.Artem")
 public class WebConfig  implements WebMvcConfigurer {
+
+    @Bean(name = "viewResolver")
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 
 
 }
---
+++ src/main/java/ru/Artem/controller/CommonController.java
@@ -0,0 +1,14 @@
+package ru.Artem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class CommonController {
+
+    @RequestMapping("/home")
+    public String getMainPage(){
+        return "main-page";
+    }
+}
